99a65b2925ea6783a4da54e469db76db02e7e1fd
David Day <David.daghero@gmail.com>
2023-04-27 17:29:12 -0300

    inicializa proyecto maven
    creacion del proyecto maven a travez de Spring Tools Suite

---
+++ Clase-10ArgJava/src/main/java/ar/edu/utn/ap4/java/Clase_10ArgJava/App.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ap4.java.Clase_10ArgJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
